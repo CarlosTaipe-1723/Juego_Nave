3c4e40e79dc458c113a5fb5514572c9990f6bbc6
Carlos Taipe <carlostaipe9025@gmail.com>
2020-05-15 04:23:41 -0500

    Proyecto al 95%

---
+++ nave.cpp
@@ -0,0 +1,258 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<list>
+using namespace std;
+
+//Estas definiciones permiten asignar el uso de las flechas
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define DERECHA 77
+#define ABAJO 80
+
+void gotoxy(int x, int y){
+	HANDLE hCon; 
+	hCon=GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = x;
+	dwPos.Y = y;
+	SetConsoleCursorPosition(hCon, dwPos);//Toma el control del cursor 
+ 
+}
+
+void OcultarCursor(){
+	HANDLE hCon; 
+	hCon=GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.dwSize = 50;//depende del valor es el estilo del cursor
+	cci.bVisible=false;//Toma dos valores true or false(False ocualta cursor)
+	//Esta funcion permite controlar las caracteristicas del cursor en patalla
+	SetConsoleCursorInfo(hCon,&cci);//el amperson es para pasar por referencia el valor
+}
+
+void Dibujar_Limites(){
+	for(int i=2;i<118;i++){
+		gotoxy(i,3);
+		printf("%c",205);
+		gotoxy(i,30);
+		printf("%c",205);
+	}
+	
+	for(int i = 4;i<30;i++){
+		gotoxy(1,i);
+		printf("%c",186);
+		gotoxy(118,i);
+		printf("%c",186);
+	}
+	
+	gotoxy(1,3); printf("%c",201);
+	gotoxy(1,30); printf("%c",200);
+	gotoxy(118,3); printf("%c",187);
+	gotoxy(118,30); printf("%c",188);
+}
+
+class NAVE{
+	 int x,y;//Variables de forma local 
+	 int corazones;
+	 int vidas;
+public:
+	NAVE(int _x, int _y, int _corazones, int _vidas):x(_x),y(_y),corazones(_corazones),vidas(_vidas){}//El constructor lleva el mismo nombre que la clase
+	int X(){return x;}
+	int Y(){return y;}
+	void COR (){corazones--;}
+	void Dibujar_Nave();
+	void Borrar_Nave();
+	void Mover_Nave();
+	void Dibujar_Corazones();
+	void Morir();	
+};
+
+
+void NAVE::Dibujar_Nave(){
+	gotoxy(x,y);   printf("  %c",30);
+	gotoxy(x,y+1); printf(" %c%c%c",40,207,41);
+	gotoxy(x,y+2); printf("%c%c %c%c",30,190,190,30);
+}
+
+void NAVE::Borrar_Nave(){
+	gotoxy(x,y);   printf("         ");
+	gotoxy(x,y+1); printf("         ");
+	gotoxy(x,y+2); printf("         ");
+}
+
+void NAVE::Mover_Nave(){
+		if(kbhit()){
+			char tecla = getch();
+			Borrar_Nave();
+			if(tecla == IZQUIERDA && x>3) 
+				x--;
+			if(tecla == DERECHA && x+6<118) 
+				x++;
+			if(tecla == ARRIBA && y>4) 
+				y--;
+			if(tecla == ABAJO && y+3<30) 
+				y++;
+			if(tecla == 'e')corazones--;
+			Dibujar_Nave();
+			Dibujar_Corazones();
+		}
+	
+}
+
+void NAVE::Dibujar_Corazones(){
+	
+	gotoxy(90,2); printf("Vidas: %d",vidas);
+	gotoxy(105,2); printf("Salud:");
+	gotoxy(111,2); printf("      ");
+	for(int i= 0;i<corazones;i++){
+		gotoxy(111+i,2);
+		printf("%c ",157);
+	}
+}
+void NAVE::Morir(){
+	if(corazones==0){
+		Borrar_Nave();
+		gotoxy(x,y);  printf("   **   ");
+		gotoxy(x,y+1);printf("  ****  ");
+		gotoxy(x,y+2);printf("   **   ");
+		Sleep(300);
+		
+		Borrar_Nave();
+		gotoxy(x,y);  printf(" * ** * ");
+		gotoxy(x,y+1);printf("  ****  ");
+		gotoxy(x,y+2);printf(" * ** * ");
+		Sleep(300);
+		Borrar_Nave();
+		
+		vidas--;
+		corazones=3;
+		Dibujar_Corazones();
+		Dibujar_Nave();
+	}
+}
+
+class AST{
+	int x,y;
+public:
+	AST(int _x, int _y): x( _x),y( _y){}
+	void Dibujar_Ast();
+	void Mover_Ast();
+	void choque( NAVE &N);
+	int X(){return x;}//Permite obtener las coordenads de los asteroides en X
+	int Y(){return y;}//Permite obtener las coordenads de los asteroides en y
+};
+
+void AST::Dibujar_Ast(){
+	gotoxy(x,y); printf(" %c ",184);
+}
+
+void AST::Mover_Ast(){
+	gotoxy(x,y); printf("  ");
+	y++;
+	if(y>29){
+		x = rand()%113+4;
+		y = 4;
+	}
+	Dibujar_Ast();
+}
+
+void AST::choque(NAVE &N){
+	if(x>=N.X()&&x<N.X()+8 && y>=N.Y() && y<=N.Y()+2){
+		N.COR();
+		N.Borrar_Nave();
+		N.Dibujar_Nave();
+		N.Dibujar_Corazones();
+		x = rand()%113+4;
+		y = 4;
+	}
+}
+
+class BALA{
+	int x,y;
+public:
+	BALA(int _x, int _y):x(_x),y(_y){}
+	int X(){return x;}//Permite obtener las coordenads de los asteroides en X
+	int Y(){return y;}//Permite obtener las coordenads de los asteroides en y
+	void Mover();
+	bool Fuera();
+};
+
+void BALA::Mover(){
+	gotoxy(x,y); printf(" ");
+	y--;
+	gotoxy(x,y); printf("*");
+}
+
+bool BALA::Fuera(){
+	if(y==4)return true;
+	return false;
+}
+
+int main (){
+	
+	OcultarCursor(); 
+	Dibujar_Limites();
+	NAVE N(37,26,3,3);//Define donde va a aparecer la nave,corazones,vidas
+	
+	N.Dibujar_Nave();
+	N.Dibujar_Corazones();
+	
+	list<AST*>A;
+	list<AST*>::iterator itA;
+	for(int i=0;i<5;i++){
+		A.push_back(new AST(rand()%114+3,rand()%5+4)); //el primer rand es para el ancho y el segundo es donde va a aparecer inicialmente el asteroide
+	}
+	
+	list<BALA*>B;//Esto es un puntero
+	list<BALA*>::iterator it;
+	
+	bool game_over = false;
+	while(!game_over){
+		
+		if(kbhit()){
+			char tecla = getch();
+			if(tecla=='a')
+			B.push_back(new BALA(N.X()+2,N.Y()-1)); 
+		}
+		
+		for(it = B.begin(); it!=B.end();it++){
+			(*it)-> Mover ();
+			
+			if((*it)->Fuera()){
+				gotoxy((*it)->X(),(*it)->Y());//Esto ayuda a obtener la coordenada
+				printf(" ");//Este espacio es para que la bala se borre
+				delete(*it);
+				it=B.erase(it);//Con esta linea quitamos un espacio y pasamos el iterador a la siguiente linea 
+			}
+		}
+		
+		for(itA=A.begin(); itA!=A.end();itA++){
+			(*itA)->Mover_Ast();
+			(*itA)->choque(N);
+		}
+		
+		for(itA = A.begin(); itA != A.end(); itA++){//Recorre los asteroides
+			for(it = B.begin(); it != B.end(); it++){//Recorre las balas 
+				if((*itA)->X() == (*it)->X() &&(
+					(*itA)->Y()+1 == (*it)->Y() ||
+					(*itA)->Y() == (*it)->Y()))
+					{
+						gotoxy((*it)->X(),(*it)->Y()); printf("   ");				
+						delete(*it);
+						it=B.erase(it);//pasa al siguiente elemento de la lista 
+						
+						A.push_back(new AST(rand() %110+5, 4));
+						gotoxy((*itA)->X(),(*itA)->Y()); printf("   ");				
+						delete(*itA);//Borra el objeto de la lista
+						itA = A.erase(itA);
+					}
+			}
+		}
+				
+		N.Morir();
+		N.Mover_Nave();
+		Sleep(30);
+	}
+	return 0;
+}
